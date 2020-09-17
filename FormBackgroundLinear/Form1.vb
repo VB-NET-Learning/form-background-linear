b091a918275875a7c6377b38eec04b460a0b3a8a
D3M6 <demg.working@gmail.com>
2020-09-17 20:27:44 +0200

    DrawGradientVertical params values changed

--- FormBackgroundLinear/Form1.vb
+++ FormBackgroundLinear/Form1.vb
@@ -5,7 +5,7 @@
     '*/
 
     Private Sub Frm_Bg_Linear_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
-        DrawGradientVertical(Color.FromArgb(61, 61, 61), Color.FromArgb(41, 41, 41))
+        DrawGradientVertical(Me.CreateGraphics, Me.DisplayRectangle, Color.FromArgb(61, 61, 61), Color.FromArgb(41, 41, 41))
     End Sub
 
     '/*
@@ -16,13 +16,14 @@
     '* DRAUW FUNCTION
     '*/
 
-    Private Sub DrawGradientVertical(ByVal vTopColor As Color, ByVal vBottomColor As Color)
+    Private Sub DrawGradientVertical(ByVal vGraphics As Graphics, ByVal vRectangle As Rectangle, ByVal vTopColor As Color, ByVal vBottomColor As Color)
         ' Initial params
-        Dim objGraphics As Graphics = Me.CreateGraphics
+        Dim objGraphics As Graphics
         Dim objBrush As Drawing2D.LinearGradientBrush
         Dim objRectangle As Rectangle
         ' Change val
-        objRectangle = Me.DisplayRectangle
+        objGraphics = vGraphics
+        objRectangle = vRectangle
         objBrush = New Drawing2D.LinearGradientBrush(objRectangle, vTopColor, vBottomColor, Drawing2D.LinearGradientMode.Vertical)
         ' Fill valor
         objGraphics.FillRectangle(objBrush, objRectangle)
