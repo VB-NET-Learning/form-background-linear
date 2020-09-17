436a443b1ce0a504f1cf7c697f46ce017ae22c0e
D3M6 <demg.working@gmail.com>
2020-09-17 19:38:06 +0200

    Added - Gradient Background(Vertical)

--- FormBackgroundLinear/Form1.vb
+++ FormBackgroundLinear/Form1.vb
@@ -1,5 +1,34 @@
 ﻿Public Class Frm_Bg_Linear
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    '/*
+    '* WINFORM LOAD
+    '*/
+
+    Private Sub Frm_Bg_Linear_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        DrawGradientVertical(Color.FromArgb(61, 61, 61), Color.FromArgb(41, 41, 41))
     End Sub
+
+    '/*
+    '*  END - WINFORM LOAD
+    '*/
+
+    '/*
+    '* DRAUW FUNCTION
+    '*/
+
+    Private Sub DrawGradientVertical(ByVal vTopColor As Color, ByVal vBottomColor As Color)
+        ' Initial params
+        Dim objGraphics As Graphics = Me.CreateGraphics
+        Dim objBrush As Drawing2D.LinearGradientBrush
+        Dim objRectangle As Rectangle
+        ' Change val
+        objRectangle = Me.DisplayRectangle
+        objBrush = New Drawing2D.LinearGradientBrush(objRectangle, vTopColor, vBottomColor, Drawing2D.LinearGradientMode.Vertical)
+        ' Fill valor
+        objGraphics.FillRectangle(objBrush, objRectangle)
+        ' Dispose objs
+        objBrush.Dispose()
+        objGraphics.Dispose()
+    End Sub
+
 End Class
